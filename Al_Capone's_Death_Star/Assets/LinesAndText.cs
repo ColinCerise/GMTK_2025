ecd7031cf902dc31732409e055f0d7156aa83eca
Terayal <alextodd2008@gmail.com>
2025-07-30 16:25:26 -0700

    All connections added

--- Al_Capone's_Death_Star/Assets/LinesAndText.cs
+++ Al_Capone's_Death_Star/Assets/LinesAndText.cs
@@ -18,11 +18,23 @@ public class LinesAndText : MonoBehaviour
     public LineRenderer Line2;
     public GameObject Connectpoint2a;
     public GameObject Connectpoint2b;
+    public LineRenderer Line3;
+    public GameObject Connectpoint3a;
+    public GameObject Connectpoint3b;
+    public LineRenderer Line4;
+    public GameObject Connectpoint4a;
+    public GameObject Connectpoint4b;
+    public LineRenderer Line5;
+    public GameObject Connectpoint5a;
+    public GameObject Connectpoint5b;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Line1 = GameObject.Find("Line1").GetComponent<LineRenderer>();
         Line2 = GameObject.Find("Line2").GetComponent<LineRenderer>();
+        Line3 = GameObject.Find("Line3").GetComponent<LineRenderer>();
+        Line4 = GameObject.Find("Line4").GetComponent<LineRenderer>();
+        Line5 = GameObject.Find("Line5").GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -32,6 +44,12 @@ public class LinesAndText : MonoBehaviour
         Line1.SetPosition(1, Connectpoint1b.transform.position);
         Line2.SetPosition(0, Connectpoint2a.transform.position);
         Line2.SetPosition(1, Connectpoint2b.transform.position);
+        Line3.SetPosition(0, Connectpoint3a.transform.position);
+        Line3.SetPosition(1, Connectpoint3b.transform.position);
+        Line4.SetPosition(0, Connectpoint4a.transform.position);
+        Line4.SetPosition(1, Connectpoint4b.transform.position);
+        Line5.SetPosition(0, Connectpoint5a.transform.position);
+        Line5.SetPosition(1, Connectpoint5b.transform.position);
 
         AccumulatedTime += Time.deltaTime;
         if (Minutes < 10)
