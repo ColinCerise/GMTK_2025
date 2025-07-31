4eca5ad720adf9134403bb523f76eddd99fea47d
Holland Young <hollandkariyoung@gmail.com>
2025-07-31 12:54:44 -0700

    Try to make a logbook

--- Al_Capone's_Death_Star/Assets/Scripts/ConvoLog.cs
+++ Al_Capone's_Death_Star/Assets/Scripts/ConvoLog.cs
@@ -1,14 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ConvoLog : MonoBehaviour
 {
     [SerializeField] List<string> conversationsLogged = new List<string>();
-    [SerializeField] 
+    [SerializeField] GameObject logbookDisplay;
+    private bool noEntries;
+    private string defaultText = "No entries available (listen in on conversations and they will be recorded here for your reference).";
+    private TextMeshProUGUI currLog;
+    private int logCounter;
+
+    private void Start()
+    {
+        logbookDisplay.SetActive(false);
+        currLog = logbookDisplay.GetComponentInChildren<TextMeshProUGUI>();
+        conversationsLogged.Clear();
+        conversationsLogged.Add(defaultText);
+        noEntries = true;
+    }
 
     public void AddConvo(string convo)
     {
+        if (noEntries)
+        {
+            noEntries = false;
+            conversationsLogged.Clear();
+        }
         conversationsLogged.Add(convo);
+    }
+
+    public void LogbookDisplay()
+    {
+        logbookDisplay.SetActive(!logbookDisplay.activeSelf);
+
+        currLog.text = conversationsLogged[0];
+        logCounter = 0;
+    }
+
+    public void ProgressLogbook()
+    {
+        if (logbookDisplay.activeSelf && logCounter < conversationsLogged.Count - 1)
+        {
+            logCounter++;
+            currLog.text = conversationsLogged[logCounter];
+        }
+    }
+
+    public void AntiProgressLogbook()
+    {
+        if (logbookDisplay.activeSelf && logCounter > 0)
+        {
+            logCounter--;
+            currLog.text = conversationsLogged[logCounter];
+        }
     }
 }
