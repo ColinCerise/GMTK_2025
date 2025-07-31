ffcd58017278018628444fa19c159b6fcd15380f
Terayal <alextodd2008@gmail.com>
2025-07-30 17:42:29 -0700

    Conversation Module coming along and bugfix for wiretap overlapping

--- Al_Capone's_Death_Star/Assets/ConversationManager.cs
+++ Al_Capone's_Death_Star/Assets/ConversationManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ConversationManager : MonoBehaviour
 {
+    public string Conversation;
+    public string ConversationTargets;
+    public float TimeOffsetHours = 12;
+    public float TimeOffsetMinutes = 10;
+    public float LPS = 20;
+    public int PlaceInConversation = 0;
+    public char CurrentChar;
+    public GameObject Wiretap;
+    public Wiretap WiretapScript;
+    public GameObject Manager;
+    public LinesAndText ManagerScript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Wiretap = GameObject.Find("Wiretap");
+        Manager = GameObject.Find("Manager");
+        WiretapScript = Wiretap.GetComponent<Wiretap>();
+        ManagerScript = Manager.GetComponent<LinesAndText>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (ManagerScript.Hours >= TimeOffsetHours && ManagerScript.Minutes >= TimeOffsetMinutes)
+        {
+
+        }
     }
 }
