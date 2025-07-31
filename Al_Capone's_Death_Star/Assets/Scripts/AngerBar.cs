b75a4539cc3c32e7a524e43290b834c233517c71
Terayal <alextodd2008@gmail.com>
2025-07-31 14:02:24 -0700

    Reformatted and anger added

--- Al_Capone's_Death_Star/Assets/Scripts/AngerBar.cs
+++ Al_Capone's_Death_Star/Assets/Scripts/AngerBar.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AngerBar : MonoBehaviour
 {
+    public float Anger = 0;
+    public float MaxAnger = 100;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class AngerBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Anger >= MaxAnger)
+        {
+            Debug.Log("GameOver");
+        }
+    }
+    public void AddAnger(float anger)
+    {
+        Anger += anger;
+    }
+    public void DisconnectedCall()
+    {
+        Anger += MaxAnger / 2;
     }
 }
